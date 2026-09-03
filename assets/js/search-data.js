d29e3879d25b33f63750ce685093e0bee762e89c
Kevin Lee <236516808+kevinjimlee@users.noreply.github.com>
2026-09-03 13:18:18 -0400

    Create search-data.js

---
+++ assets/js/search-data.js
@@ -0,0 +1,38 @@
+window.SEARCH_INDEX = [
+  {
+    title: "About",
+    type: "Page",
+    url: "/#about",
+    excerpt: "TEST TEST TEST"
+  },
+  {
+    title: "News",
+    type: "Page",
+    url: "/#news",
+    excerpt: "I HAVE NO NEWS"
+  },
+  {
+    title: "Projects",
+    type: "Page",
+    url: "/#projects",
+    excerpt: "IT'S ONLY SMH."
+  },
+  {
+    title: "Blog",
+    type: "Page",
+    url: "/blog.html",
+    excerpt: "Rambles."
+  },
+  {
+    title: "TEST TEST TEST",
+    type: "Post",
+    url: "/article.html?post=blog",
+    excerpt: "TEST."
+  },
+  {
+    title: "AGHHHH,
+    type: "Project",
+    url: "/article.html?project=semi-online",
+    excerpt: "AGHH."
+  }
+];
